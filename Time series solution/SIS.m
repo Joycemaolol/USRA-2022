76ea6160dd8b9ed3ee2fb224f644a244ef5235ca
Joycemaolol <91842873+Joycemaolol@users.noreply.github.com>
2022-08-27 14:36:46 -0400

    Add files via upload

---
+++ Time series solution/SIS.m
@@ -0,0 +1,17 @@
+function [sol] = SIS(t, init, theta)
+    R_0 = theta(1);
+    gamma = theta(2);
+    mu = theta(3);
+    epsilon = 1;
+    beta = R_0*(gamma+mu);
+    q = gamma/(gamma+mu);
+    S = init(1);
+    I = init(2);
+    R = init(3);
+    %dydt = zeros(size(y))
+    lambda = beta*I;
+    sol = [mu-(mu+lambda)*S,
+        lambda*S+epsilon*lambda*R-mu*I-gamma*I,
+        gamma*I-mu*R-epsilon*lambda*R];
+    
+end
